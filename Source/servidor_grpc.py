b0c7e858bfa7a5532efd6c4f32ffebb265830c53
acarmonag <acarmonag@eafit.edu.co>
2023-08-28 15:18:42 -0500

    Se eliminan debuggs y se soluciona el error

--- Source/servidor_grpc.py
+++ Source/servidor_grpc.py
@@ -21,9 +21,6 @@ class ProductService(Service_pb2_grpc.ProductServiceServicer):
             ruta_archivo = os.path.join(RUTA_ARCHIVOS, archivo)
             fecha_modificacion = datetime.datetime.fromtimestamp(os.path.getmtime(ruta_archivo)).strftime('%Y-%m-%d %H:%M:%S')
             tamaño = os.path.getsize(ruta_archivo) / (1024*1024) # Tamaño en MB
-            print("Archivo encontrado: " + archivo)
-            print("Última modificación: " + fecha_modificacion)
-            print("Tamaño: " + str(tamaño) + " MB")
             archivo_respuesta.append(Service_pb2.singleTransactionResponse(nombre=archivo, last_updated=fecha_modificacion, size=tamaño))
             yield Service_pb2.multipleTransactionResponse(files=archivo_respuesta)
             
